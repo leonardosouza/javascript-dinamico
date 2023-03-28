3fb728f32faba9e0c5dc89a22db63d1c5d2de093
Leonardo Souza <leonardodisouza@gmail.com>
2023-03-27 21:56:51 -0300

    aula 07

--- aula-06/cep.js
+++ aula-06/cep.js
@@ -10,13 +10,70 @@ function onlyNumbers(e) {
     this.value = this.value.replace(/\D+/g, "");
 }
 
-function validateEntry() {
-
+function validateEntry() {    
+    if (this.value.length === 8) {
+        // this.style.borderColor = ""; 
+        // this.style.borderWidth = "";
+        // this.style.backgroundColor = "";
+        this.classList.remove("error");
+        getAddress(this.value);
+    } else {        
+        // this.style.borderColor = "red";
+        // this.style.borderWidth = "2px";
+        // this.style.backgroundColor = "yellow";
+        this.classList.add("error");
+        this.focus();
+    }
 }
 
-function getAddress() {
+function getAddress(postalCode) {
+    // endpoint
+    // const endpoint = "https://viacep.com.br/ws/" + postalCode + "/json/";
+    const endpoint = `https://viacep.com.br/ws/${postalCode}/json/`;
     
+    // config
+    const config = {
+        method: "GET"
+    };
+
+    // request
+    /*
+    
+                  Promise
+                    |
+                 <pending>
+                /         \
+           fulfilled     rejected
+           .then()       .catch()
+    */
+    fetch(endpoint, config)
+        .then(function(resp) { return resp.json(); })
+        .then(getAddressSuccess)
+        .catch(getAddressError);
 }
+
+function getAddressSuccess(address) {
+    const { logradouro, cep, localidade, uf, bairro } = address;
+
+    const card = `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${logradouro}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">
+                            ${bairro} - ${localidade} - ${uf}
+                        </h6>
+                        <p class="card-text">${cep}</p>
+                    </div>
+                </div>`;
+
+    document.querySelector(".cards").innerHTML = card;
+}
+
+function getAddressError() {
+    console.log("deu ruim 1!");
+}
+
+
 
 // Mapping Events
 document.querySelector("#cep").addEventListener("input", onlyNumbers); // onlyNumbers(InputEvent)
+document.querySelector("#cep").addEventListener("focusout", validateEntry);
